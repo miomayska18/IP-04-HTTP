d5faa08b6d48b104e079da990898d12f695d9f02
Magdalena <miomayska18@codingburgas.bg>
2022-09-30 23:51:02 +0300

    Complete task 5

---
+++ MiniServer.Demo/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using MiniServer.HTTP;
+using MiniServer.WebServer;
+using System.IO;
+using System.Text;
+
+namespace MiniServer.Demo
+{
+    public class HomeController
+    {
+        public IHttpResponse Index(IHttpRequest request)
+        {
+            string content = "<h1>Hello, World!</h1>";
+            return new HtmlResult(content, HttpResponseStatusCode.Ok);
+        }
+    }
+}
---
+++ MiniServer.Demo/Launcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MiniServer.HTTP;
+using MiniServer.WebServer;
+
+namespace MiniServer.Demo
+{
+    public class Launcher
+    {
+        public static void Main(string[] args)
+        {
+            IServerRoutingTable serverRoutingTable = new ServerRoutingTable();
+
+            serverRoutingTable.Add(
+                HttpRequestMethod.Get,
+                path:"/",
+                func:request => new HomeController().Index(request));
+
+            Server server = new Server(8080, serverRoutingTable);
+
+            server.Run();
+        }
+    }
+}
